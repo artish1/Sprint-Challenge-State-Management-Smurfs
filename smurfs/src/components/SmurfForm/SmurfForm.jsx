f93a427c62098eb4d514768c66e3022c64e381c1
Mark Artishuk <markyshuk@gmail.com>
2019-11-08 09:22:38 -0800

    Form now adds smurf to db and properly displays added smurf

--- smurfs/src/components/SmurfForm/SmurfForm.jsx
+++ smurfs/src/components/SmurfForm/SmurfForm.jsx
@@ -2,11 +2,17 @@ import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+import { addSmurf } from "../../actions/smurfActions";
+//Redux
+import { useDispatch } from "react-redux";
 
 const useStyles = makeStyles(theme => ({
   container: {
     backgroundColor: "#f5f5f5",
     maxWidth: 1000,
+    padding: "25px 10px",
     margin: "0 auto"
   },
   textField: {
@@ -36,6 +42,7 @@ const initialFormData = {
 
 const SmurfForm = () => {
   const [formData, setFormData] = useState(initialFormData);
+  const dispatch = useDispatch();
   const classes = useStyles();
 
   const handleOnChange = e => {
@@ -47,16 +54,24 @@ const SmurfForm = () => {
 
   const handleSubmit = e => {
     e.preventDefault();
-    console.log("Submitted with data: ", formData);
+    dispatch(addSmurf(formData));
+    setFormData(initialFormData);
   };
 
   return (
-    <form className={classes.container} noValidate autoComplete="off">
+    <form
+      onSubmit={handleSubmit}
+      className={classes.container}
+      noValidate
+      autoComplete="off"
+    >
+      <Typography variant="h5" component="h2">
+        Add New Smurf
+      </Typography>
       <div className={classes.textFields}>
         <TextField
           id="outlined-basic"
           name="name"
-          id="name"
           className={classes.textField}
           label="Smurf Name"
           placeholder="Name"
@@ -70,7 +85,6 @@ const SmurfForm = () => {
           className={classes.textField}
           label="Age"
           name="age"
-          id="age"
           placeholder="Age"
           margin="normal"
           variant="outlined"
@@ -82,7 +96,6 @@ const SmurfForm = () => {
           className={classes.textField}
           label="Height"
           name="height"
-          id="height"
           placeholder="Height"
           margin="normal"
           variant="outlined"
@@ -90,7 +103,12 @@ const SmurfForm = () => {
           onChange={handleOnChange}
         />
       </div>
-      <Button variant="contained" color="primary" className={classes.button}>
+      <Button
+        type="submit"
+        variant="contained"
+        color="primary"
+        className={classes.button}
+      >
         Add Smurf
       </Button>
     </form>
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,7 +1,8 @@
 import {
   FETCH_SMURFS_FAIL,
   FETCH_SMURFS_START,
-  FETCH_SMURFS_SUCCESS
+  FETCH_SMURFS_SUCCESS,
+  ADD_SMURF_SUCCESS
 } from "../actions/smurfActions";
 
 const initialState = {
@@ -31,6 +32,8 @@ const initialState = {
 
 export const smurfReducer = (state = initialState, action) => {
   switch (action.type) {
+    case ADD_SMURF_SUCCESS:
+      return { ...state, smurfs: action.payload };
     case FETCH_SMURFS_START:
       return { ...state, isFetching: true, error: "" };
     case FETCH_SMURFS_SUCCESS: // { type: FETCH_SMURFS_SUCCESS, payload: res.data}
