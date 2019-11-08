ed5114c293dc484365d4f11fd8bb8e05cbaaa6d6
Mark Artishuk <markyshuk@gmail.com>
2019-11-08 08:18:37 -0800

    Added action case statements to reducer

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,9 +1,23 @@
+import {
+  FETCH_SMURFS_FAIL,
+  FETCH_SMURFS_START,
+  FETCH_SMURFS_SUCCESS
+} from "../actions/smurfActions";
+
 const initialState = {
-  smurfs: []
+  smurfs: [],
+  isFetching: false,
+  error: ""
 };
 
 export const smurfReducer = (state, action) => {
   switch (action.type) {
+    case FETCH_SMURFS_START:
+      return { ...state, isFetching: true, error: "" };
+    case FETCH_SMURFS_SUCCESS: // { type: FETCH_SMURFS_SUCCESS, payload: res.data}
+      return { ...state, isFetching: false, smurfs: action.payload, error: "" };
+    case FETCH_SMURFS_FAIL:
+      return { ...state, isFetching: false, error: action.payload };
     default:
       return state;
   }
